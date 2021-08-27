a6f20da1cd054e5bf6ce6aa8054ffad61afe1f7e
samiranGithub <samirankumarroy24@gmail.com>
2021-08-27 14:25:58 +0600

    first commit

---
+++ js/package.js
@@ -0,0 +1,129 @@
+const qualificationInfo = [
+    {
+        name: "Samiran Kumar Roy.",
+        age: 21,
+        dob: "17/04/2000.",
+        address: {
+            vill: "Khagrabari.,",
+            dist: "Coochbehar,",
+            state: "West Bengal,",
+            country: "India,",
+            pin: "736179.",
+        },
+        nationality: "Indian.",
+        phone: "+91-8016249231",
+        email: "samirankumarroy24@gmail.com"
+
+    }
+]
+
+const portfolioData = [
+    {
+        id: 1,
+        category: "portfolio_modal",
+        img: "portfolio-image/portfolio-demo1.jpg",
+        title: "This is first Portfolio Website",
+        desc1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque sollicitudin quam, vel pulvinar leo tempus at. Aliquam eu nisl felis. Vivamus sodales dolor ut ante vehicula fermentum. Curabitur pretium sapien quis odio aliquet aliquam. Nullam odio odio, dignissim nec nisi ac, commodo finibus leo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla egestas leo lectus, id laoreet ligula interdum vel. Donec ante ipsum, tempus quis justo at, commodo iaculis lorem. Quisque nec elementum nisi, et molestie mi. Sed id condimentum nisl. Nulla vel ullamcorper dui. Nam mi arcu, lobortis at urna eget, iaculis pharetra lorem. In fringilla urna sed ultricies bibendum. Phasellus elementum quam a felis porttitor condimentum quis vel nisl. Sed ante est, vulputate eu auctor ut, commodo quis magna. Nunc vulputate rhoncus nibh, id sollicitudin justo scelerisque vel.`,
+        desc2: `bbbhsbvhbsvbasvlnkbsavksadvhbv. hjkhfdgovhdsiass`,
+        desc3: `my name is mihir kanti roy . i am from aditmary`
+    },
+    {
+        id: 2,
+        category: "portfolio_modal",
+        img: "portfolio-image/portfolio-demo2.jpg",
+        title: "This is Two Portfolio Website",
+        desc1: `lorem10fvhvvhffhhfvhfvjkbbvhbdvbdfibv. iohdov`,
+        desc2: `bbbhsbvhbsvbasvlnkbsavksadvhbv. hjkhfdgovhdsiass`,
+        desc3: `my name is mihir kanti roy . i am from aditmary`
+    },
+    {
+        id: 3,
+        category: "portfolio_modal",
+        img: "portfolio-image/portfolio-demo3.jpg",
+        title: "This is Third Portfolio Website",
+        desc1: `lorem10fvhvvhffhhfvhfvjkbbvhbdvbdfibv. iohdov`,
+        desc2: `bbbhsbvhbsvbasvlnkbsavksadvhbv. hjkhfdgovhdsiass`,
+        desc3: `my name is mihir kanti roy . i am from aditmary`
+    },
+    {
+        id: 4,
+        category: "hh_modal",
+        img: "portfolio-image/hh1.jpg",
+        title: "This is first Hotel & Hostel Website",
+        desc1: `lorem10fvhvvhffhhfvhfvjkbbvhbdvbdfibv. iohdov`,
+        desc2: `bbbhsbvhbsvbasvlnkbsavksadvhbv. hjkhfdgovhdsiass`,
+        desc3: `my name is mihir kanti roy . i am from aditmary`
+    },
+    {
+        id: 5,
+        category: "hh_modal",
+        img: "portfolio-image/hh2.jpg",
+        title: "This is two Hotel & Hostel Website",
+        desc1: `lorem10fvhvvhffhhfvhfvjkbbvhbdvbdfibv. iohdov`,
+        desc2: `bbbhsbvhbsvbasvlnkbsavksadvhbv. hjkhfdgovhdsiass`,
+        desc3: `my name is mihir kanti roy . i am from aditmary`
+    },
+    {
+        id: 6,
+        category: "hh_modal",
+        img: "portfolio-image/hh3.jpg",
+        title: "This is third Hotel & Hostel Website",
+        desc1: `lorem10fvhvvhffhhfvhfvjkbbvhbdvbdfibv. iohdov`,
+        desc2: `bbbhsbvhbsvbasvlnkbsavksadvhbv. hjkhfdgovhdsiass`,
+        desc3: `my name is mihir kanti roy . i am from aditmary`
+    },
+    {
+        id: 7,
+        category: "ecommerce_modal",
+        img: "portfolio-image/ecommerce1.jpg",
+        title: "This is first ecommerce Website",
+        desc1: `lorem10fvhvvhffhhfvhfvjkbbvhbdvbdfibv. iohdov`,
+        desc2: `bbbhsbvhbsvbasvlnkbsavksadvhbv. hjkhfdgovhdsiass`,
+        desc3: `my name is mihir kanti roy . i am from aditmary`
+    },
+    {
+        id: 8,
+        category: "ecommerce_modal",
+        img: "portfolio-image/ecommerce2.jpg",
+        title: "This is two ecommerce Website",
+        desc1: `lorem10fvhvvhffhhfvhfvjkbbvhbdvbdfibv. iohdov`,
+        desc2: `bbbhsbvhbsvbasvlnkbsavksadvhbv. hjkhfdgovhdsiass`,
+        desc3: `my name is mihir kanti roy . i am from aditmary`
+    },
+    {
+        id: 9,
+        category: "ecommerce_modal",
+        img: "portfolio-image/ecommerce3.jpg",
+        title: "This is third ecommerce Website",
+        desc1: `lorem10fvhvvhffhhfvhfvjkbbvhbdvbdfibv. iohdov`,
+        desc2: `bbbhsbvhbsvbasvlnkbsavksadvhbv. hjkhfdgovhdsiass`,
+        desc3: `my name is mihir kanti roy . i am from aditmary`
+    },
+    {
+        id: 10,
+        category: "blog_modal",
+        img: "portfolio-image/blog1.jpg",
+        title: "This is one blog Website",
+        desc1: `lorem10fvhvvhffhhfvhfvjkbbvhbdvbdfibv. iohdov`,
+        desc2: `bbbhsbvhbsvbasvlnkbsavksadvhbv. hjkhfdgovhdsiass`,
+        desc3: `my name is mihir kanti roy . i am from aditmary`
+    },
+    {
+        id: 11,
+        category: "blog_modal",
+        img: "portfolio-image/blog2.jpg",
+        title: "This is two blog Website",
+        desc1: `lorem10fvhvvhffhhfvhfvjkbbvhbdvbdfibv. iohdov`,
+        desc2: `bbbhsbvhbsvbasvlnkbsavksadvhbv. hjkhfdgovhdsiass`,
+        desc3: `my name is mihir kanti roy . i am from aditmary`
+    },
+    {
+        id: 12,
+        category: "blog_modal",
+        img: "portfolio-image/blog3.jpg",
+        title: "This is third blog Website",
+        desc1: `lorem10fvhvvhffhhfvhfvjkbbvhbdvbdfibv. iohdov`,
+        desc2: `bbbhsbvhbsvbasvlnkbsavksadvhbv. hjkhfdgovhdsiass`,
+        desc3: `my name is mihir kanti roy . i am from aditmary`
+    }
+];
